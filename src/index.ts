c7d6d27f8e49181eb82b2266db3296791c68ccaf
LanzoMA <lanzo.ma9@gmail.com>
2024-11-17 09:22:47 +0000

    completed ts config

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,6 @@ const PORT: number = 5000;
 
 app.use(express.json());
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello World!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
