b1019c62aca1044f8097d6c9635ca2ea09a7261d
LanzoMA <lanzo.ma9@gmail.com>
2024-11-28 08:50:13 +0000

    added achievement model

---
+++ src/db/models/achievements.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+interface AchievementInput {
+    name: string;
+    description: string;
+}
+
+interface AchievementDocument extends Document, AchievementInput { }
+
+const achievementSchema: Schema<AchievementDocument> = new Schema<AchievementDocument>({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+const Achievement: Model<AchievementDocument> = mongoose.model<AchievementDocument>('Achievement', achievementSchema);
+
+export { Achievement, AchievementInput };
