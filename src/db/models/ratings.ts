6e6968da5aad89eb2a21a83859eed92887d7b615
LanzoMA <lanzo.ma9@gmail.com>
2025-01-05 11:01:07 +0000

    implemented rating model

---
+++ src/db/models/ratings.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+
+interface RatingInput {
+    userId: string;
+    questionId: string;
+    difficulty: number;
+    marks: number;
+}
+
+interface RatingDocument extends Document {
+    user: Schema.Types.ObjectId;
+    question: Schema.Types.ObjectId;
+    difficulty: number;
+    marks: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const ratingSchema: Schema<RatingDocument> = new Schema<RatingDocument>({
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    question: { type: Schema.Types.ObjectId, required: true, ref: 'Question' },
+    difficulty: { type: Number, required: true },
+    marks: { type: Number, required: true },
+}, { timestamps: true });
+
+const Rating: Model<RatingDocument> = mongoose.model('Rating', ratingSchema);
+
+export { Rating, RatingInput };
