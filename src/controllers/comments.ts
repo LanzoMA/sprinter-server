e52affc0471d40d47b6e235f2f83bed81062bbd3
LanzoMA <lanzo.ma9@gmail.com>
2025-01-05 09:58:22 +0000

    implemented comments controller

---
+++ src/controllers/comments.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { createComment, getComments } from '../db/services/comments';
+
+export const createCommentHandler = async (req: Request, res: Response) => {
+    const user = req.body.user;
+    const question = req.params.question;
+    const comment = req.body.comment;
+
+    try {
+        await createComment({ userId: user.id, questionId: question, comment });
+        res.sendStatus(201);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+}
+
+export const getCommentsHandler = async (req: Request, res: Response) => {
+    const question = req.params.question;
+
+    try {
+        const comments = await getComments(question);
+        res.json(comments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+}
