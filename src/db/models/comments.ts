0628ceab1850930ca94ddc3e4e4884dc05f83b92
LanzoMA <lanzo.ma9@gmail.com>
2025-01-05 09:18:17 +0000

    created comment model

---
+++ src/db/models/comments.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+
+interface CommentInput {
+  user: Schema.Types.ObjectId;
+  question: Schema.Types.ObjectId;
+  comment: string;
+}
+
+interface CommentDocument extends Document, CommentInput {
+  createdAt: Date;
+}
+
+const commentSchema: Schema<CommentDocument> = new Schema<CommentDocument>({
+  user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+  question: { type: Schema.Types.ObjectId, required: true, ref: 'Question' },
+  comment: { type: String, required: true },
+}, { timestamps: true });
+
+const Comment: Model<CommentDocument> = mongoose.model('Comment', commentSchema);
+
+export { Comment, CommentInput };
