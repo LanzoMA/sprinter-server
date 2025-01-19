a343e6610c53ab3b516ff69efd888627b5622a90
LanzoMA <lanzo.ma9@gmail.com>
2025-01-19 11:39:09 +0000

    modified question model

--- src/db/models/questions.ts
+++ src/db/models/questions.ts
@@ -1,34 +1,25 @@
-import mongoose, { Schema, Document, Model } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 interface QuestionInput {
     question: string;
     markScheme: string;
-    course: Schema.Types.ObjectId;
-    year?: number;
-    questionNumber?: number;
+    title: string;
+    description: string;
+    course: string;
     totalMarks: number;
-    author: Schema.Types.ObjectId
+    author: string;
 }
 
-interface QuestionDocument extends Document, QuestionInput {
-    averageDifficulty?: number;
-    averageMark?: number;
-    createdAt: Date;
-}
-
-const questionSchema: Schema<QuestionDocument> = new Schema<QuestionDocument>({
+const questionSchema = new Schema({
     question: { type: String, required: true },
     markScheme: { type: String, required: true },
     course: { type: Schema.Types.ObjectId, required: true },
-    year: { type: Number },
-    questionNumber: { type: Number },
+    title: { type: String, required: true },
+    description: { type: String },
     totalMarks: { type: Number, required: true },
-    averageDifficulty: { type: Number },
-    averageMark: { type: Number },
     author: { type: Schema.Types.ObjectId, required: true },
-    createdAt: { type: Date, default: Date.now(), required: true },
 });
 
-const Question: Model<QuestionDocument> = mongoose.model<QuestionDocument>('Question', questionSchema);
+const Question = mongoose.model('Question', questionSchema);
 
 export { Question, QuestionInput };
