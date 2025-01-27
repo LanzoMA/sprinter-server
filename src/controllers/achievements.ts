e2dbf7bd48a865f6adbd942d4315d1897acaf320
LanzoMA <lanzo.ma9@gmail.com>
2025-01-27 12:30:27 +0000

    removed manual validation

--- src/controllers/achievements.ts
+++ src/controllers/achievements.ts
@@ -4,11 +4,6 @@ import { createAchievement } from '../db/services/achievements';
 export const createAchievementHandler = async (req: Request, res: Response) => {
     const { name, description } = req.body;
 
-    if (!name || !description) {
-        res.status(400).send('Name/description fields were not given.');
-        return;
-    }
-
     try {
         await createAchievement({ name, description });
         res.sendStatus(201);
