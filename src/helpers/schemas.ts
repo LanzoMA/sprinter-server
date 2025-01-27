488a63ef6330070886f0828d7150c0b088562b1c
LanzoMA <lanzo.ma9@gmail.com>
2025-01-27 12:14:54 +0000

    removed question search validation

--- src/helpers/schemas.ts
+++ src/helpers/schemas.ts
@@ -9,15 +9,6 @@ const questionSchema = Joi.object({
     author: Joi.string().required(),
 });
 
-const searchSchema = Joi.object({
-    course: Joi.string().required(),
-    difficulty: Joi.string().required(),
-    minMarks: Joi.number().required(),
-    maxMarks: Joi.number().required(),
-    sortBy: Joi.string().required(),
-});
-
 export default {
     '/questions': questionSchema,
-    '/questions/search': searchSchema,
 } as { [key: string]: ObjectSchema };
