d58774c3be2f5dd98dc814266621ae01cd959039
LanzoMA <lanzo.ma9@gmail.com>
2025-01-27 14:37:05 +0000

    using url params

--- src/controllers/ratings.ts
+++ src/controllers/ratings.ts
@@ -4,9 +4,10 @@ import { createRating } from '../db/services/ratings';
 
 const createRatingHandler = async (req: Request, res: Response) => {
     const user: UserToken = req.body;
-    const { question, difficulty, marks } = req.body;
+    const { id } = req.params;
+    const { difficulty, marks } = req.body;
 
-    await createRating({ user: user.id, question, difficulty, marks });
+    await createRating({ user: user.id, question: id, difficulty, marks });
     res.sendStatus(201);
 };
 
