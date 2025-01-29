0e9a241399308f5df5030d9e31cb1509dad99ee8
LanzoMA <lanzo.ma9@gmail.com>
2025-01-29 14:09:06 +0000

    added user achievement model

---
+++ src/db/models/user-achievements.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+
+interface UserAchievementInput {
+    user: string;
+    achievement: string;
+}
+
+interface UserAchievementDocument extends Document {
+    user: Schema.Types.ObjectId;
+    achievement: Schema.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const userAchievementSchema: Schema<UserAchievementDocument> =
+    new Schema<UserAchievementDocument>(
+        {
+            user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+            achievement: {
+                type: Schema.Types.ObjectId,
+                ref: 'Achievement',
+                required: true,
+            },
+        },
+        { timestamps: true }
+    );
+
+const UserAchievement: Model<UserAchievementDocument> =
+    mongoose.model<UserAchievementDocument>(
+        'UserAchievement',
+        userAchievementSchema
+    );
+
+export { UserAchievement, UserAchievementInput };
