9918c4b02eb7d224be163746fdcf13422c8a258b
LanzoMA <lanzo.ma9@gmail.com>
2025-02-04 19:43:39 +0000

    removed unnecessary date interface

--- src/helpers/models.ts
+++ src/helpers/models.ts
@@ -6,36 +6,4 @@ interface SearchQuery {
     sortBy?: string;
 }
 
-interface IDate {
-    year: number;
-    month: number;
-    day: number;
-}
-
-const getCurrentDate = (): IDate => {
-    const date = new Date();
-
-    return {
-        year: date.getFullYear(),
-        month: date.getMonth(),
-        day: date.getDate(),
-    };
-};
-
-const convertDate = (date: Date): IDate => {
-    return {
-        year: date.getFullYear(),
-        month: date.getMonth(),
-        day: date.getDate(),
-    };
-};
-
-const isSameDate = (firstDate: IDate, secondDate: IDate): boolean => {
-    return (
-        firstDate.year === secondDate.year &&
-        firstDate.month === secondDate.month &&
-        firstDate.day === secondDate.day
-    );
-};
-
-export { SearchQuery, IDate, getCurrentDate, convertDate, isSameDate };
+export { SearchQuery };
