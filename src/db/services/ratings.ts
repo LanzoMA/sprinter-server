a4e7ffe3e99b38bd2e3ae16887b025fbd91011b2
LanzoMA <lanzo.ma9@gmail.com>
2025-02-04 19:44:10 +0000

    implemented get daily streak service

--- src/db/services/ratings.ts
+++ src/db/services/ratings.ts
@@ -1,6 +1,7 @@
+import { isSameDate, toDateFormat } from '../../helpers/date';
 import { Rating, RatingInput } from '../models/ratings';
 
-export const createRating = async (rating: RatingInput) => {
+const createRating = async (rating: RatingInput) => {
     const existingRating = await Rating.findOne({
         user: rating.user,
         question: rating.question,
@@ -17,3 +18,49 @@ export const createRating = async (rating: RatingInput) => {
         marks: rating.marks,
     }).save();
 };
+
+const getDailyStreak = async (user: string): Promise<number> => {
+    const ratings = await Rating.find({ user }, { _id: 0, createdAt: 1 }).sort({
+        createdAt: -1,
+    });
+
+    let streak = 0;
+
+    if (ratings.length === 0) return streak;
+
+    // Get the days when the user has been active
+    const activeDays: Array<Date> = Array.from(
+        new Set(ratings.map((rating) => toDateFormat(rating['createdAt'])))
+    ).map((date) => new Date(date));
+
+    const today = new Date();
+
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+
+    // Check if a streak has begun i.e. they have been activity today or yesterday
+    if (
+        !isSameDate(today, activeDays[0]) &&
+        !isSameDate(yesterday, activeDays[0])
+    ) {
+        return 0;
+    }
+
+    // Get streak starting point
+    const current = isSameDate(today, activeDays[0])
+        ? new Date(today)
+        : new Date(yesterday);
+
+    streak++;
+    let end = false;
+
+    // Count the number of continuous days since the starting point
+    while (streak < activeDays.length && !end) {
+        current.setDate(current.getDate() - 1);
+        isSameDate(current, activeDays[streak]) ? streak++ : (end = true);
+    }
+
+    return streak;
+};
+
+export { createRating, getDailyStreak };
